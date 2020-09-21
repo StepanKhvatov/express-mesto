f65a8cc35715ea2870414d1ad10c721644c92be8
Stepan Khvatov <stepanhv@yandex.ru>
2020-09-22 01:12:28 +0700

    project 13./gitfix

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,4 @@
 const cards = require('express').Router();
-<<<<<<< HEAD
 const {
   getAllCards,
   createCard,
@@ -18,10 +17,4 @@ cards.put('/cards/:cardId/likes', likeCard);
 
 cards.delete('/cards/:cardId/likes', dislikeCard);
 
-=======
-const { getAllCards } = require('../controllers/cards');
-
-cards.get('/cards', getAllCards);
-
->>>>>>> b8d7ba23d867dcc0cf0b3b113c50ddd951581e43
 module.exports = cards;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const users = require('express').Router();
-<<<<<<< HEAD
 const {
   getAllUsers,
   getUserById,
@@ -17,12 +16,5 @@ users.post('/users', createUser);
 users.patch('/users/me', updateUser);
 
 users.patch('/users/me/avatar', updateAvatar);
-=======
-const { getAllUsers, getUserById } = require('../controllers/users');
-
-users.get('/users', getAllUsers);
-
-users.get('/users/:id', getUserById);
->>>>>>> b8d7ba23d867dcc0cf0b3b113c50ddd951581e43
 
 module.exports = users;
